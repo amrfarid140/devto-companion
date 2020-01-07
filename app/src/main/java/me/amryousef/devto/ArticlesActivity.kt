14023a9d4f3b6718d9f7023915de754207839791
Amr Yousef <amr.farid140@gmail.com>
2020-01-07 13:40:16 +0000

    created base layouts

---
+++ app/src/main/java/me/amryousef/devto/ArticlesActivity.kt
@@ -0,0 +1,12 @@
+package me.amryousef.devto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArticlesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_articles)
+    }
+}

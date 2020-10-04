8d67e42d48ea3fddeb068f7105c5d5c0fc2f86b1
Amr Yousef <amr.farid140@gmail.com>
2020-10-04 20:13:38 +0100

    BuildSrc

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,32 @@
+object Versions {
+    const val composeVersion = "1.0.0-alpha04"
+    const val kotlinVersion = "1.4.10"
+    const val coroutinesVersion = "1.3.9-native-mt-2"
+    const val ktorVersion = "1.4.1"
+    const val serializationVersion = "1.0.0-RC2"
+}
+
+object Dependencies {
+    val jetpackCompose =
+        mapOf(
+            "ui" to "androidx.compose.ui:ui:${Versions.composeVersion}",
+            "tooling" to "androidx.ui:ui-tooling:${Versions.composeVersion}",
+            "foundation" to "androidx.compose.foundation:foundation:${Versions.composeVersion}",
+            "materialDesign" to "androidx.compose.material:material:${Versions.composeVersion}"
+        )
+
+    const val kotlinStd = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val coil = "io.coil-kt:coil:0.11.0"
+    const val androidCore = "androidx.core:core-ktx:1.3.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val materialComponents = "com.google.android.material:material:1.1.0"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
+    const val ktorCore = "io.ktor:ktor-client-core:${Versions.ktorVersion}"
+    const val ktorCIO = "io.ktor:ktor-client-cio:${Versions.ktorVersion}"
+    const val ktorAndroid = "io.ktor:ktor-client-android:${Versions.ktorVersion}"
+    const val ktoriOS = "io.ktor:ktor-client-ios:${Versions.ktorVersion}"
+    const val ktorJson = "io.ktor:ktor-client-json:${Versions.ktorVersion}"
+    const val ktorSerialization = "io.ktor:ktor-client-serialization:${Versions.ktorVersion}"
+    const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serializationVersion}"
+}
---
+++ data/src/androidMain/kotlin/api/KtorDispatcher.kt
@@ -0,0 +1,5 @@
+package api
+
+import kotlinx.coroutines.Dispatchers
+
+actual val KtorDispatcher = Dispatchers.Default
